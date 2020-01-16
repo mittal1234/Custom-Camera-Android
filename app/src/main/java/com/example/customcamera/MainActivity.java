1206f5492c3eb215687ef5e656986768a70d869a
shubham_mittal_mobcoder <shubham.mittal@mobcoder.com>
2020-01-16 17:25:53 +0530

    implementation of eye detection

--- app/src/main/java/com/example/customcamera/MainActivity.java
+++ app/src/main/java/com/example/customcamera/MainActivity.java
@@ -16,21 +16,32 @@ import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.util.Log;
 import android.util.Size;
 import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -47,6 +58,16 @@ public class MainActivity extends AppCompatActivity {
 
     private AppCompatButton button;
     private TextureView textureView;
+    AppCompatTextView textView;
+
+
+    private static final String TAG = "MainActivity";
+
+    CameraSource cameraSource;
+
+    ArrayAdapter adapter;
+    ArrayList<String> list = new ArrayList<>();
+
 
     //check state orientation of output image
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
@@ -101,7 +122,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textureView = findViewById(R.id.btnCamera);
+        textView = findViewById(R.id.textView);
 
+        cameraFacedetcetion();
         assert textureView != null;
         textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
             @Override
@@ -124,6 +147,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
 
         button = findViewById(R.id.btnCapture);
 
@@ -380,5 +405,106 @@ public class MainActivity extends AppCompatActivity {
         mBackgroundThread.start();
         mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
     }
+
+    private void cameraFacedetcetion(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+            Toast.makeText(this, "Grant Permission and restart app", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+            createCameraSource();
+        }
+    }
+    public void createCameraSource() {
+        FaceDetector detector = new FaceDetector.Builder(this)
+                .setTrackingEnabled(true)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .setMode(FaceDetector.FAST_MODE)
+                .build();
+        detector.setProcessor(new MultiProcessor.Builder(new FaceTrackerFactory()).build());
+
+        cameraSource = new CameraSource.Builder(this, detector)
+                .setRequestedPreviewSize(1024, 768)
+                .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                .setRequestedFps(30.0f)
+                .build();
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            cameraSource.start();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private class FaceTrackerFactory implements MultiProcessor.Factory<Face> {
+
+        private FaceTrackerFactory() {
+
+        }
+
+        @Override
+        public Tracker<Face> create(Face face) {
+            return new EyesTracker();
+        }
+    }
+
+    private class EyesTracker extends Tracker<Face> {
+
+        private final float THRESHOLD = 0.75f;
+
+        private EyesTracker() {
+
+        }
+
+        @Override
+        public void onUpdate(Detector.Detections<Face> detections, Face face) {
+
+
+            if (face.getIsLeftEyeOpenProbability() > THRESHOLD || face.getIsRightEyeOpenProbability() > THRESHOLD) {
+                Log.i(TAG, "onUpdate: Eyes Detected");
+                showStatus("Eyes Detected and open, so video continues");
+
+
+            }
+            else {
+
+                Log.i(TAG, "onUpdate: EyesClosed");
+                showStatus("Eyes Detected and closed, so video paused");
+            }
+        }
+
+        @Override
+        public void onMissing(Detector.Detections<Face> detections) {
+            super.onMissing(detections);
+            showStatus("Face Not Detected yet!");
+        }
+
+        @Override
+        public void onDone() {
+            super.onDone();
+        }
+    }
+
+    public void showStatus(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(message);
+            }
+        });
+    }
+
+
 
 }
